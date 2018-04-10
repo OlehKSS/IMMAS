06bdfcdab238c42fb1640f98b79d108775820873
briannaburton1 <briannaburton@gmail.com>
2018-04-10 13:16:02 +0200

    creation of accuracy estimation function

---
+++ AccuracyEstimation/FindAccuracy.py
@@ -0,0 +1,12 @@
+"""
+Write a function which finds the accuracy of a given output image given the image and the ground truth
+Based on Alessandro Bria's C++ file compute-segmentation-accuracy.cpp
+"""
+
+def accuracy (segemented_image):
+
+
+
+
+
+    return accuracy_est;
