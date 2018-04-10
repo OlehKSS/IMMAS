84778d7b9a1787040eb92468e7b70a5b0ee1f370
briannaburton1 <briannaburton@gmail.com>
2018-04-10 14:20:35 +0200

    work on accuracy

--- AccuracyEstimation/FindAccuracy.py
+++ AccuracyEstimation/FindAccuracy.py
@@ -1,9 +1,14 @@
 """
 Write a function which finds the accuracy of a given output image given the image and the ground truth
 Based on Alessandro Bria's C++ file compute-segmentation-accuracy.cpp
+
+Accuracy (ACC) is defined as:
+    ACC = (True positives + True negatives)/(number of samples)
+    i.e., as the ratio between the number of correctly classified samples (in our case, pixels)
+    and the total number of samples (pixels)
 """
 
-def accuracy (segemented_image):
+def accuracy (segemented_images,groundtruth_images,):
 
 
 
