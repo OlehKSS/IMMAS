e559bcd750ad1ca22664006a52ffdcae33e60abe
Mahlet <mahleta.b735@gmail.com>
2018-04-17 10:04:42 +0200

    Preprocessing functions

--- immas/preprocessing.py
+++ immas/preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-<<<<<<< HEAD
+
 # Contrast Limited Adaptive Histogram Equalization (CLAHE)
 # An algorithm for local contrast enhancement, that uses histograms computed over different tile regions of the image.
 # Local details can therefore be enhanced even in regions that are darker or lighter than most of the image.
@@ -56,4 +56,4 @@ def clahe (img, clip=10.0, grid=(8,8)):
     clahe = cv2.createCLAHE(clip,grid)
     new_img = clahe.apply(img)
     return new_img
->>>>>>> 0905ca9701e0c45c6788d89410dc6aa711adf882
+
