c6a0d04c045171a91f9d3464feb69b92a4f39c82
marcioabcr <32570044+marcioabcr@users.noreply.github.com>
2018-04-17 11:21:06 +0200

    Branches

--- immas/preprocessing.py
+++ immas/preprocessing.py
@@ -18,3 +18,21 @@ def clahe (img, clip=10.0, grid=(8,8)):
     clahe = cv2.createCLAHE(clip,grid)
     new_img = clahe.apply(img)
     return new_img
+
+
+def fullPreprocessing (img):
+    '''
+    Applies Limited Adaptive Histogram Equalization (CLAHE) to an image.
+    Local details are enhanced even in regions that are darker or lighter than most of the image.
+        
+    Args:
+        img (uint8): image file.
+        clip (float): contrast limit (default = 10.0). The pixels above are clipped and distributed uniformly to other bins before applying histogram equalization.        
+        grid (tuple): size of the block (default = (8,8)) of the image where histogram equalization is going to be performed.
+
+    Returns:
+        (new_img): image obtained after CLAHE application. 
+    '''
+
+    new_img = clahe(img)
+    return new_img
