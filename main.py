ce22cf30711cee1e2cab59885a7e55ab1ad49594
Mahlet <mahleta.b735@gmail.com>
2018-04-20 13:27:29 +0200

    Wavelet Transform test added

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import pywt
 
 PATH = './dataset/images/'
 MASK = './dataset/masks/'
 image_name='50993841_de5e8d61e501a71b_MG_L_CC_ANON.tif'
 image_mask='50993841_de5e8d61e501a71b_MG_L_CC_ANON.mask.png'
-
+import math
 
 def resize(image):
 	small = cv2.resize(image, (0,0), fx=0.25, fy=0.25)
@@ -46,21 +47,36 @@ if __name__== "__main__":
 
 	#apply contrast enhacement
 	contrasted = clahe(im,20,8);
-	cv2.imshow('Improved_image(CLAHE)',contrasted) 
+	#cv2.imshow('Improved_image(CLAHE)',contrasted) 
 
+	titles = ['Approximation', ' Horizontal detail',
+	          'Vertical detail', 'Diagonal detail']
+	coeffs2 = pywt.dwt2(contrasted, 'bior1.3')
+	LL, (LH, HL, HH) = coeffs2
+	fig = plt.figure()
+	for i, a in enumerate([LL, LH, HL, HH]):
+	    ax = fig.add_subplot(2, 2, i + 1)
+	    ax.imshow(a, origin='image', interpolation="nearest", cmap=plt.cm.gray)
+	    plt.show()
+	    ax.set_title(titles[i], fontsize=12)
+	  
+	fig.suptitle("dwt2 coefficients", fontsize=14)
 
-	eroded = cv2.morphologyEx(im, cv2.MORPH_ERODE, kernel) 
-	cv2.imshow('eroded',eroded)
+	print ("After loop")
+	LL = math.sqrt(2)*LL
+	LH = math.sqrt(2)*LH
+	HL = math.sqrt(2)*HL
+	HH = math.sqrt(2)*HH
+	coeffs2 = LL, (LH, HL, HH) 
+	plt.imshow(contrasted, cmap="gray")
+	plt.axis('off')
+	plt.title('Before wavelet transform')
+	plt.show()
 
-	closed = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
-	cv2.imshow('closed',closed) 
-
-	opened = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel)
-	cv2.imshow('opened',opened)
-
-	dilate = cv2.morphologyEx(im, cv2.MORPH_DILATE, kernel)
-	cv2.imshow('opened',opened)
-
+	reconstructed = pywt.idwt2(coeffs2, 'bior1.3')
+	plt.imshow(reconstructed, interpolation="nearest", cmap=plt.cm.gray)
+	plt.title('After wavelet transform')
+	plt.show()
 
 	cv2.waitKey(0) 
 
