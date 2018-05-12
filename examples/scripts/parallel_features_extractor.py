edf366291bc8e250524802e8ab5ab7db9f7d6339
Oleh Kozynets <ok7mailbox@gmail.com>
2018-05-12 18:50:32 +0200

    Data analysis added.

--- examples/scripts/parallel_features_extractor.py
+++ examples/scripts/parallel_features_extractor.py
@@ -1,24 +1,33 @@
 import os.path as path
+import time
 
 from immas.io import read_dataset
 from immas.features.get_features import get_dataset_features_parallel
 
 path_immas = "/home/okozyn/Projects/immas/"
-path_dataset = "/home/okozyn/Projects/immas/dataset/"
+path_dataset = path.join(path_immas, "dataset")
 
-data_set = read_dataset(image_folder=path.join(path_dataset, "masses_examples"),
+data_set = read_dataset(image_folder=path.join(path_dataset, "images"),
             mask_folder=path.join(path_dataset, "masks"),
-            results_folder=path.join(path_dataset, "groundtruth"),
-            pmuscle_mask_folder=path.join(path_dataset, "pectoral_muscle_masks"), train_set_fraction=1)
+            results_folder=path.join(path_dataset, "groundtruth"), 
+            train_set_fraction=1)
 
 print("Number of images for training is {0}, number of images for testing is {1}".format(
     len(data_set["train"]), len(data_set["test"])))
 
-imgs = data_set["train"]
-
-print("Started feature extraction.")
-features = get_dataset_features_parallel(imgs, contour_max_number=None)
+print("Started train feature extraction.")
+features = get_dataset_features_parallel(data_set["train"], contour_max_number=None)
+#print("Started test feature extraction.")
+#features_test = get_dataset_features_parallel(data_set["test"], contour_max_number=None, train=False)
 
 # this will delete all rows whith NaN values in it and save our data to a separate file
 features = features.dropna()
-features.to_csv(path.join(path_immas, "/debug/classifier-train-data.csv"))
+#features_test = features_test.dropna()
+
+#timestamp for file name
+time_stamp = int(time.time())
+file_name_train = f"examples/feature-tables/train-data_{time_stamp}.csv"
+file_name_test = f"examples/feature-tables/test-data_{time_stamp}.csv"
+
+features.to_csv(path.join(path_immas, file_name_train))
+#features_test.to_csv(path.join(path_immas, file_name_test))
