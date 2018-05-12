ccb912241e5d1cd6b2664b74868b5f78fb54b32f
Oleh Kozynets <ok7mailbox@gmail.com>
2018-05-12 16:49:08 +0200

    Fixes #103. Changes fo feature detection, incorporation of Dice index.

--- immas/features/get_features.py
+++ immas/features/get_features.py
@@ -10,11 +10,19 @@ from .binarization import get_candidates_mask
 from .geometry import get_geom_features
 from .intensity import get_itensity_features
 
-def get_img_features(img, mask_ground_truth=None, contour_max_number=10, train=True):
+from ..constants import DICE_INDEX_DEFAULT_THRESHOLD, CLASS_ID_FPR, CLASS_ID_TPR, MIN_ROI_AREA
+from ..classification import get_rois
+
+
+def get_img_features(img,
+                     mask_ground_truth=None,
+                     contour_max_number=10,
+                     train=True,
+                     dice_threshold=DICE_INDEX_DEFAULT_THRESHOLD):
     '''
     Function calculates features of the given image. Class id for the true positive is 1,
     and for the false positive (not masses) -1. Regions of interest that have area less than
-    625 (25 by 25) will be ignored.
+    625 will be ignored.
 
     Args:
         img (numpy.array): image, which features to find
@@ -25,90 +33,96 @@ def get_img_features(img, mask_ground_truth=None, contour_max_number=10, train=T
         train (bool): if True, dataframe of features will be returned with correct class
         ids assigned to each candidate region, if False all class ids will be zero. Default
         value is True.
+        dice_threshold (int): threshold for Dice index, all regions that higher than this
+        threshold will be considered as massses.
 
     Returns:
-        (pandas.DataFrame, [opencv.contour]): features of selected contours 
-        and list of contours.   
+        (pandas.DataFrame, [dict], [dict], [str]): features of selected contours, list of
+        true positive regions of interest, list of false positive regions of interest, list of
+        feature names. Each region is represented as an instance of class dict with fields:
+        {
+            "class_id: 1 for a mass, -1 for non-mass,
+            "contour": opencv defined contour,
+            "dice_index": maximum of the Dice index for the region,
+            "features": numpy array of features
+        } 
     '''
 
     img_thresh = get_candidates_mask(img)
 
     number_of_masses = 0
-    min_area = 25 * 25
+    min_area = MIN_ROI_AREA
 
-    if train and (not (mask_ground_truth is None)):
-        # delete mass region from the mask
-        _, mask_ground_truth = cv2.threshold(mask_ground_truth, 0, 1, cv2.THRESH_BINARY)
-        img_thresh = img_thresh * (1 - mask_ground_truth)
+    if train:
+        id_tpr = CLASS_ID_TPR
+        id_fpr = CLASS_ID_FPR
+    else:
+        id_tpr = 0
+        id_fpr = 0
 
-        _, mass_contours, _ = cv2.findContours(mask_ground_truth,
-                                               cv2.RETR_EXTERNAL,
-                                               cv2.CHAIN_APPROX_SIMPLE)
+    regions_tpr, regions_fpr = get_rois(img_thresh, mask_ground_truth, dice_threshold)
 
-        number_of_masses = len(mass_contours) 
+    number_of_masses = len(regions_tpr)
 
-    _, contours, _ = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # select only contours with area higher than min area
-    contours = [c for c in contours if cv2.contourArea(c) > min_area]
+    regions_fpr = [c for c in regions_fpr if cv2.contourArea(c["contour"]) > min_area]
     # sort obtained contours by area in descending order
-    contours.sort(key=lambda c: cv2.contourArea(c), reverse=True)
+    regions_fpr.sort(key=lambda c: cv2.contourArea(c["contour"]), reverse=True)
 
     # how many contours should we investigate
-    if (contour_max_number is None) or (len(contours) < contour_max_number):
+    if (contour_max_number is None) or (len(regions_fpr) < contour_max_number):
         # in case we do not have enough contours in our image
         # or we do not want to limit the number of contours to select
         # we do not need to change controus array in this case
-        contour_max_number = len(contours)
+        contour_max_number = len(regions_fpr)
     else:
         # select biggest contours, according to provided max number of contours
-        contours = contours[:contour_max_number]
+        regions_fpr = regions_fpr[:contour_max_number]
 
-    geom_features = get_geom_features(contours[0])
-    intens_features = get_itensity_features(img, contours[0])
-    features = list(geom_features.values()) + list(intens_features.values())
-    features_names = list(geom_features.keys()) + list(intens_features.keys())
-    # append class identificator
-    features_names.append("class_id")
-    # size of features array:
-    # rows = number of non-mass contours + number of mass contours
-    # cols = number of features + 1 (for class id)
-    arr_features = np.zeros((contour_max_number + number_of_masses, 
-                            len(features) + 1), 
-                            dtype=float)
-    arr_features[0, :-1] = features
-    # since we work here with false positives
-    arr_features[0, -1] = -1
+    for index, region in enumerate(regions_fpr):
+        contour = region["contour"]
 
-    for index, contour in enumerate(contours):
-        if index != 0:
+        if index == 0:
+            geom_features = get_geom_features(contour)
+            intens_features = get_itensity_features(img, contour)
+
+            features = list(geom_features.values()) + list(intens_features.values())
+            features_names = list(geom_features.keys()) + list(intens_features.keys())
+            # append class identificator
+            features_names.append("class_id")
+            # size of features array:
+            # rows = number of non-mass contours + number of mass contours
+            # cols = number of features + 1 (for class id)
+            arr_features = np.zeros((contour_max_number + number_of_masses,
+                                    len(features) + 1),
+                                    dtype=float)
+        else:
             geom_features = get_geom_features(contour)
             intens_features = get_itensity_features(img, contour)
             features = list(geom_features.values()) + list(intens_features.values())
 
-            arr_features[index, :-1] = features
-            # since we work here with false positives
-            arr_features[index, -1] = -1
+        region["features"] = features
+        arr_features[index, :-1] = features
+        # since we work here with false positives
+        arr_features[index, -1] = id_fpr
 
-    if train and (not (mask_ground_truth is None)):
-        # appends mass contours, true positive
-        for index, contour in enumerate(mass_contours):
-                geom_features = get_geom_features(contour)
-                intens_features = get_itensity_features(img, contour)
-                features = list(geom_features.values()) + list(intens_features.values())
-                # we append this data to the tail, so we need to add number of contours 
-                # added previously
-                arr_features[contour_max_number + index, :-1] = features
-                # since we work here with false positives
-                arr_features[contour_max_number + index, -1] = 1
+    for index, region in enumerate(regions_tpr):
+        contour = region["contour"]
 
-        # add groundtruth contours to the output as well
-        contours = contours + mass_contours        
+        geom_features = get_geom_features(contour)
+        intens_features = get_itensity_features(img, contour)
+        features = list(geom_features.values()) + list(intens_features.values())
 
-    if not train:
-        # class ids should be 0 if we need to return test data frame of features
-        arr_features[:, -1] = 0
+        region["features"] = features
+        # we append this data to the tail, so we need to add number of contours 
+        # added previously
+        arr_features[contour_max_number + index, :-1] = features
+        # since we work here with false positives
+        arr_features[contour_max_number + index, -1] = id_tpr
 
-    return (DataFrame(arr_features, columns=features_names), contours)
+    features_df = DataFrame(arr_features, columns=features_names)
+    # class_id will be excluded from feature names in the output
+    return (features_df, regions_tpr, regions_fpr, features_names[:-1])
 
 
 def get_dataset_features(data, contour_max_number=10, train=True):
@@ -133,14 +147,14 @@ def get_dataset_features(data, contour_max_number=10, train=True):
     for mm in data:
         try:
             mm.read_data()
-            features, _ = mm.get_img_features(contour_max_number, train=train)
+            features = mm.get_img_features(contour_max_number, train=train)
             feat_data_frames.append(features)
         except Exception as e:
             print(f"Caught an exception, mammogram {mm.file_name}")
             print(type(e))
             print(e.args)
             print(e)
-            print(traceback.format_exc())  
+            print(traceback.format_exc())
 
     return concat(feat_data_frames, ignore_index=True)
 
@@ -165,7 +179,7 @@ def get_dataset_features_parallel(data, contour_max_number=10, train=True, proce
 
     pool = mp.Pool(processes=processes)
 
-    feat_data_frames = [pool.apply(helper,args=(mm, contour_max_number, train)) for mm in data] 
+    feat_data_frames = [pool.apply(helper,args=(mm, contour_max_number, train)) for mm in data]
 
     return concat(feat_data_frames, ignore_index=True)
 
@@ -174,7 +188,7 @@ def helper(img, contour_max_number, train):
     '''Helper function for running dataset feature extraction in parallel.'''
     try:
         img.read_data()
-        features, _ = img.get_img_features(contour_max_number, train=train)
+        features = img.get_img_features(contour_max_number, train=train)
 
         return features
 
@@ -183,4 +197,4 @@ def helper(img, contour_max_number, train):
         print(type(e))
         print(e.args)
         print(e)
-        print(traceback.format_exc()) 
+        print(traceback.format_exc())
