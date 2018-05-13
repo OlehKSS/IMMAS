637b4541c6ee4325a4c486ec7d6d1c65b0ba43b0
Mahlet <mahleta.b735@gmail.com>
2018-05-13 22:42:29 +0200

    SVM notebook added

--- immas/segmentation.py
+++ immas/segmentation.py
@@ -210,12 +210,8 @@ def fullSegmentation(img):
     img = mean_shift(img, 20, 20)
     img = multithresholding(img)
     img = thresh_to_binary(img)
-<<<<<<< HEAD
-    img = preprocessing.open(img, (20, 20))
- 
-    return img
-=======
+
     img = preprocessing.open(img, (23, 23))
     img = preprocessing.close(img,(5,5))
     return img
->>>>>>> master
+
